31b8b5a7d9bd4792de49707ffa80359decbc3466
kkrogs <kourtney.krogman@gmail.com>
2022-05-21 17:46:59 -0600

    more changes

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -13,7 +13,7 @@ const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
   const userData = data?.me || {};
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  // create function that accepts the books mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
