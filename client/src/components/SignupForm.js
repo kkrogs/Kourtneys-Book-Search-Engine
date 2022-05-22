d1ef07d1063784ec51b453dd340f80f0132e9ad5
kkrogs <kourtney.krogman@gmail.com>
2022-05-21 18:20:02 -0600

    finishing code

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -43,7 +43,7 @@ const SignupForm = () => {
 
   return (
     <>
-      {/* This is needed for the validation of functionality above */}
+      {/* This is needed for the validation of functionality as seen above */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         {/* show alert if server response is bad */}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
