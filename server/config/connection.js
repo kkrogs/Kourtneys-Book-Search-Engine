a3e4a4231ec923d837c7101a661aac8dcd5032b2
kkrogs <kourtney.krogman@gmail.com>
2022-05-21 18:11:47 -0600

    connecting everything

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://nicole_w:lokiandlily2020@cluster0.hfj3i.mongodb.net/googlebooks`, {
+mongoose.connect(`mongodb://localhost:27017/myapp`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
